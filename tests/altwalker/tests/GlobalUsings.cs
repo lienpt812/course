7ab7c59be3d01283d1da4864633e22bd7b409951
lienpt812 <phamthilien08122005@gmail.com>
2026-04-29 15:40:22 +0700

    update

--- tests/altwalker/tests/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using NUnit.Framework;
-global using OpenQA.Selenium;
-global using OpenQA.Selenium.Support.UI;
-global using SeleniumExtras.WaitHelpers;
-global using CourseRegistration.Tests.Helpers;
-global using System.Text.Json;
